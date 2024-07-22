422467a418e9d1a002fef18f4780f49148d4097d
Vadim Anciferoff <sssolosnake@gmail.com>
2024-07-22 07:19:49 +0700

    ADD: baseStyle

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,18 @@
 import React from "react"
 import ReactDOM from "react-dom/client"
+import { ThemeProvider } from "styled-components"
+
 import App from "./components/App.tsx"
+import { baseTheme } from "./styles/themes/baseTheme.ts"
+import "./styles/normalize.css"
+import "./styles/fonts.css"
+import { BaseWrapperStyle } from "./styles/baseStyle.ts"
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
 	<React.StrictMode>
-		<App />
+		<ThemeProvider theme={baseTheme}>
+			<BaseWrapperStyle />
+			<App />
+		</ThemeProvider>
 	</React.StrictMode>
 )
